09a2f22539f1f8742b4f6dcebbdd34e728a2f145
CI Server <ci@example.com>
2023-03-07 18:37:27 +0000

    Site checkin for project OpenWMS.org WMS: Movements Services

---
+++ apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.openwms.wms.movements","c":"MovementsRunner","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"org.openwms.wms.movements","c":"MovementsRunner","l":"MovementsRunner()","u":"%3Cinit%3E()"}];updateSearchResults();
